81449c8793134546d343c12ba5529de53fea8297
JarbasAl <33701864+JarbasAl@users.noreply.github.com>
2026-06-13 02:17:04 +0000

    Increment Version to 0.0.1a1

--- version.py
+++ version.py
@@ -8,3 +8,18 @@ __version__ = "{}.{}.{}{}".format(VERSION_MAJOR, VERSION_MINOR, VERSION_BUILD,
 
 if __name__ == "__main__":
     print(__version__)
+# START_VERSION_BLOCK
+VERSION_MAJOR = 0
+VERSION_MINOR = 0
+VERSION_BUILD = 1
+VERSION_ALPHA = 1
+# END_VERSION_BLOCKVERSION_MAJOR = 0
+VERSION_MINOR = 0
+VERSION_BUILD = 1
+VERSION_ALPHA = 0
+
+__version__ = "{}.{}.{}{}".format(VERSION_MAJOR, VERSION_MINOR, VERSION_BUILD,
+                                   "a{}".format(VERSION_ALPHA) if VERSION_ALPHA else "")
+
+if __name__ == "__main__":
+    print(__version__)
